3614a234460ae33fe9460d03c832b6cc5f02ddb6
lluancarlo <luan_veloz@hotmail.com>
2018-09-10 02:00:00 +0000

    Update 09-sep-2018

--- bt.py
+++ bt.py
@@ -1,23 +1,21 @@
-import bluetooth
+\import bluetooth
 
-server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+class bt:
+	def __init__(self,port):
+		self.server = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+		self.server.bind(("",port))
+		self.server.listen(port)
+		self.client, self.clientAddress = self.server.accept()
+	def msg(self):
+		return self.client.recv(1024)
+	def off(self):
+		self.client.close()
+		self.server.close()
 
-server_socket.bind(("",1))
-server_socket.listen(1)
-
-client_socket, address = server_socket.accept()
-print ("Conectado ao dispositivo ",address)
-
-while 1:
-	data = client_socket.recv(1024)
-	print("Mensagem: ",data)
-
-	if data == '0':
-		client_socket.send(" Turn Off! ")
-	elif data == '1':
-		client_socket.send(" Turn On! ")
-	elif data == '2':
-		client_socket.send(" It's a Number Two boy. ")
-
-client_socket.close()
-server_socket.close()
+def receptorBluetooth():
+	data = ""
+	b = bt(1)
+	while data != '0':
+		data = b.msg()
+	else:
+		b.off()
